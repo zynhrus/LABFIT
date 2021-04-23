1fe8714e01485fc6ca7f41e8eb1c00b2d04dcdf2
Albert <albertonly898@gmail.com>
2021-04-24 00:53:13 +0700

    Initial Commit

---
+++ LABFIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LABFIT
+//
+//  Created by Albert . on 24/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
