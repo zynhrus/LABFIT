d6c81e8e4eadc8a28e1463389e2265ccf1d3c94b
zynhrus <albertonly898@gmail.com>
2021-04-25 02:05:09 +0700

    Create goals tab view

--- LABFIT/ViewController.swift
+++ LABFIT/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var countingLabel: UILabel!
+    @IBOutlet weak var heightCount: UIView!
+    @IBOutlet weak var weightCount: UIView!
+    
+    var circularProgressBarView: CircularProgressBarView!
+    var circularViewDuration: TimeInterval = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        heightCount.layer.cornerRadius = heightCount.frame.size.width / 2
+        weightCount.layer.cornerRadius = weightCount.frame.size.width / 2
+        
+        setUpCircularProgressBarView()
     }
-
-
+    
+    func setUpCircularProgressBarView() {
+        // set view
+        circularProgressBarView = CircularProgressBarView(frame: .zero)
+        // align to the center of the screen
+        circularProgressBarView.frame = CGRect(x: view.frame.width/2
+                                               , y: 200,width: 100, height: 100)
+        // call the animation with circularViewDuration
+        circularProgressBarView.progressAnimation(duration: circularViewDuration)
+        // add this view to the view controller
+        view.addSubview(circularProgressBarView)
+    }
+    
 }
 
