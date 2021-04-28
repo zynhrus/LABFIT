2c5d2bcc4784873494becf5f04d801c0919e6277
Josua David <josuadavid62@gmail.com>
2021-04-28 19:40:09 +0700

    design profile done, tinggal function

---
+++ LABFIT/profileViewController.swift
@@ -0,0 +1,77 @@
+//
+//  profileViewController.swift
+//  LABFIT
+//
+//  Created by Josua David on 28/04/21.
+//
+
+import UIKit
+
+class profileViewController: UIViewController {
+
+  
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageProfile: UIImageView!
+    @IBOutlet weak var circleButton: UIButton!
+    
+    var arrZoom: [ZoomBackground] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTable()
+        addData()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        circleButton.layer.cornerRadius = circleButton.frame.width / 2
+        circleButton.layer.masksToBounds = true
+         
+        imageProfile.layer.masksToBounds = true
+        imageProfile.layer.cornerRadius = imageProfile.bounds.width / 2
+    }
+    
+    func setupTable(){
+        let nib = UINib(nibName: "TableViewProfileCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "TableProfile1")
+    }
+    
+    func addData(){
+        let age = ZoomBackground(image: "age", tittle: "Age")
+        arrZoom.append(age)
+        let weight = ZoomBackground(image: "weight", tittle: "My Weight")
+        arrZoom.append(weight)
+        let height = ZoomBackground(image: "height", tittle: "My Height")
+        arrZoom.append(height)
+        let BMR = ZoomBackground(image: "bmr", tittle: "Calculator BMR")
+        arrZoom.append(BMR)
+        let BMI = ZoomBackground(image: "bmi", tittle: "Calculator BMI")
+        arrZoom.append(BMI)
+        
+    }
+    
+}
+
+extension profileViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrZoom.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableProfile1") as! TableViewProfileCell
+        
+        cell.zoomBackground = arrZoom[indexPath.row]
+        cell.updateUI()
+        
+        return cell
+    }
+}
+
+extension profileViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.frame.height / 6
+    
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
